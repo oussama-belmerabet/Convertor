764ab223f407515a3e5b657a471d08db996076d8
oussama-belmerabet <oussamabelmerab@gmail.com>
2023-05-11 06:47:39 +0100

    add the first label

---
+++ resources/views/header.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
+    <title>convert Coruency</title>
+</head>
+<body class='bg-gray-200 font-sans h-screen leading-none'>
+    @yield('content')
+</body>
+</html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.css')
-    <title>Document</title>
-</head>
-<body>
-    <div class="text-3xl font-bold underline">
-        hello world
+@extends('header')
+
+@section('content')
+    <div class="min-w-screen min-h-screen bg-gray-100 flex flex-col justify-center items-center">
+        <div class="w-5/6 lg:w-3/6 rounded-2xl  bg-gradient-to-b shadow-xl">
+            <div class="text-white bg-gray-200 py-4">
+                <div class=" text-center font-bold text-2xl text-blue-900">
+                    <h2 >
+                        <i class="fab fa-gg"></i>
+                        convert
+                    </h2>
+                </div>
+                <form action="/convert" method="POST">
+                    @csrf
+                    <div class="flex items-center justify-between mb-5">
+                        <div class="flex flex-col font-bold w-2/6 px-2">
+                            <label for="amount" class="mb-3 text-black">
+                                Amount
+                            </label>
+
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+        </div>
     </div>
-</body>
-</html>
+@endsection
